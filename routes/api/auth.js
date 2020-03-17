f5d67257221779a751ebe8ba96e1c17332a016c4
fbzr <fabricio.spb@gmail.com>
2020-03-16 22:37:57 -0700

    login user backend endpoint working - api/auth/login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require('../../models/User');
 
@@ -27,6 +28,52 @@ router.post('/register', async (req, res) => {
         
         // 200 response
         res.json(user);
+    } catch(err) {
+        res.status(500).send(`Server error: ${err.message}`);
+    }
+});
+
+// @route   POST api/auth/login
+// @desc    User log in - Authenticate user & get token
+// @access  Public
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid credentials'}] });
+        }
+
+        const userExists = await bcrypt.compare(password, user.password);
+
+        if (!userExists) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid credentials'}] });
+        }
+
+        // Set jwt payload
+        const jwtPayload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        // Get JWT
+        jwt.sign(
+            jwtPayload,
+            process.env.JWT_SECRET,
+            { expiresIn: 3600 }, // 1 hour 3600 seconds
+            (err, token) => {
+                if (err) throw err;
+
+                // 200 response
+                res.json({ 
+                    token,
+                    user_id: user.id
+                });
+            }
+        );
     } catch(err) {
         res.status(500).send(`Server error: ${err.message}`);
     }
