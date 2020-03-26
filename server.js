9d7d1f93883bcef67713a01d99ce40fe1788aa3c
fbzr <fabricio.spb@gmail.com>
2020-03-26 16:23:58 -0700

    prepare to deploy mern stack to heroku

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 // DEPLOYMENT - Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('/client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
