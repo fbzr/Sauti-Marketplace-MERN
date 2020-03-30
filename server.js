cdac63367cd87300a8628e5dec62c6c34c3b2485
fbzr <fabricio.spb@gmail.com>
2020-03-30 08:39:18 -0700

    deploying

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 // DEPLOYMENT - Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('/client/build'));
-
+    // Set static   
+    app.use(express.static('client/build'));
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
